5629597da590abaaaeaef7205f083d19f45a5822
ramazankarisan <rkarisan25@gmail.com>
2022-01-16 17:53:15 +0100

    hover and transition for btns and hamburger menu added

---
+++ public/index.js
@@ -0,0 +1,10 @@
+let burger = document.querySelector("#burger");
+let menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden")
+  }
+})
